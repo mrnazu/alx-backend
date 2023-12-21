048029b0eaf71dd3a8af208552e94e015ddd7cae
mrnazu <samuelamsalu1998@gmail.com>
2023-12-21 08:33:38 +0300

    3. Deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        assert isinstance(index, int) and index >= 0, "Index must be a non-negative integer"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
+
+        dataset = self.dataset()
+        total_rows = len(dataset)
+
+        # Calculate start index and next index based on the provided index and page size
+        start_index = index
+        next_index = min(index + page_size, total_rows)
+
+        # Ensure the start index is within the valid range
+        start_index = min(start_index, total_rows - 1)
+
+        # Return the dictionary with hypermedia pagination information
+        hyper_data = {
+            'index': start_index,
+            'next_index': next_index,
+            'page_size': page_size,
+            'data': dataset[start_index:next_index]
+        }
+
+        return hyper_data
+
+
+# Example usage:
+server = Server()
+
+# Example 1: Requesting index 0 with page_size 10
+hyper_data_1 = server.get_hyper_index(index=0, page_size=10)
+print(hyper_data_1)
+
+# Example 2: Requesting next index (10) with page_size 10
+# Assuming rows 3, 6, and 7 were deleted, the user should still receive rows indexed 10 to 19 included
+hyper_data_2 = server.get_hyper_index(index=10, page_size=10)
+print(hyper_data_2)
+
